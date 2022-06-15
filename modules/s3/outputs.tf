752f00d461038b800e8df9432278dd5554d1b82d
inesmarca <imarcarian@itba.edu.ar>
2022-06-15 18:02:43 -0300

    cloudfront

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -8,6 +8,11 @@ output "arn" {
     value       = aws_s3_bucket.this.arn
 }
 
+output "bucket_regional_domain_name" {
+    description = "The bucket regional domain name of the bucket. Will be of format arn:aws:s3:::bucketname"
+    value       = aws_s3_bucket.this.bucket_regional_domain_name
+}
+
 output "website_endpoint" {
     description = "The website endpoint, if the bucket is configured with a website. If not, this will be an empty string"
     value       = aws_s3_bucket.this.website_endpoint

5994ab90e89266ccc69919fe503613fca6947ba9
Alan Sartorio <alan42ga@hotmail.com>
2022-06-20 23:13:59 -0300

    Code format.

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -1,24 +1,24 @@
 output "id" {
-    description = "The bucket domain name. Will be of format bucketname.s3.amazonaws.com"
-    value       = aws_s3_bucket.this.id
+  description = "The bucket domain name. Will be of format bucketname.s3.amazonaws.com"
+  value       = aws_s3_bucket.this.id
 }
 
 output "arn" {
-    description = "The ARN of the bucket. Will be of format arn:aws:s3:::bucketname"
-    value       = aws_s3_bucket.this.arn
+  description = "The ARN of the bucket. Will be of format arn:aws:s3:::bucketname"
+  value       = aws_s3_bucket.this.arn
 }
 
 output "bucket_regional_domain_name" {
-    description = "The bucket regional domain name of the bucket. Will be of format arn:aws:s3:::bucketname"
-    value       = aws_s3_bucket.this.bucket_regional_domain_name
+  description = "The bucket regional domain name of the bucket. Will be of format arn:aws:s3:::bucketname"
+  value       = aws_s3_bucket.this.bucket_regional_domain_name
 }
 
 output "website_endpoint" {
-    description = "The website endpoint, if the bucket is configured with a website. If not, this will be an empty string"
-    value       = aws_s3_bucket_website_configuration.this.website_endpoint
+  description = "The website endpoint, if the bucket is configured with a website. If not, this will be an empty string"
+  value       = aws_s3_bucket_website_configuration.this.website_endpoint
 }
 
 output "oai" {
-    description = "The website endpoint, if the bucket is configured with a website. If not, this will be an empty string"
-    value       = aws_cloudfront_origin_access_identity.this
+  description = "The website endpoint, if the bucket is configured with a website. If not, this will be an empty string"
+  value       = aws_cloudfront_origin_access_identity.this
 }
--- modules/vpc/gateways.tf
+++ modules/vpc/gateways.tf
@@ -1,38 +1,38 @@
 # Internet Gateway
 resource "aws_internet_gateway" "this" {
-    vpc_id = aws_vpc.this.id
+  vpc_id = aws_vpc.this.id
 
-    tags = {
-        Name = "internet-gateway"
-    }
+  tags = {
+    Name = "internet-gateway"
+  }
 }
 
 # NAT Gateway
 resource "aws_eip" "this" {
-    for_each = aws_subnet.public
-    vpc = true
+  for_each = aws_subnet.public
+  vpc      = true
 
-    lifecycle {
-        prevent_destroy = true
-    }
+  lifecycle {
+    prevent_destroy = true
+  }
 
-    tags = {
-        Name = format("eip-%s", each.key)
-    }
+  tags = {
+    Name = format("eip-%s", each.key)
+  }
 }
 
 resource "aws_nat_gateway" "this" {
-    for_each      = aws_subnet.public
-    subnet_id     = each.value.id
-    allocation_id = aws_eip.this[each.key].id
-    
-    # To ensure proper ordering, it is recommended to add an explicit dependency
-    # on the Internet Gateway for the VPC.
-    depends_on = [aws_internet_gateway.this]
+  for_each      = aws_subnet.public
+  subnet_id     = each.value.id
+  allocation_id = aws_eip.this[each.key].id
 
-    tags = {
-        Name = format("nat-gateway-%s", each.key)
-    }
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.this]
+
+  tags = {
+    Name = format("nat-gateway-%s", each.key)
+  }
 }
 
 
